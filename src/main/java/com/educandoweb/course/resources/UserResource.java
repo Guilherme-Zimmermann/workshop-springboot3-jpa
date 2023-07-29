d0eb1d64bd3e6103a97e1c9089fdde99044d2c17
GuiWill06 <zimmerma83@gmail.com>
2023-07-29 10:50:59 -0400

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Will", "will@gmail.com", "999999999", "12345");
+		User u = new User(1L, "Gui", "will@gmail.com", "999999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
