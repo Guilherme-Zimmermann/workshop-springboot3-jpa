ec8bf76dd1400d54c2e67a51bffa38cc2e5dde76
GuiWill06 <zimmerma83@gmail.com>
2023-08-01 12:35:15 -0400

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -87,17 +87,25 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	public Set<OrderItem> getItems() {
-		return items;
-	}
-	
+
 	public Payment getPayment() {
 		return payment;
 	}
 	
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
